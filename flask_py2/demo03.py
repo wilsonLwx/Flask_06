b3388abf3d1a0520b7b20d52dd9198270a2baf8a
wilsonLWX <15000108650@163.com>
2018-03-24 10:39:02 +0800

    add

--- flask_py2/demo03.py
+++ flask_py2/demo03.py
@@ -7,12 +7,26 @@ from flask_migrate import Migrate, MigrateCommand
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI '] = 'mysql://root:mysql@127.0.0.1/flask_py2'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS '] = False
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:mysql@127.0.0.1/flask_py2'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 app.debug = True
 manager = Manager(app)
 db = SQLAlchemy(app)
+
+
+class Role(db.Model):
+    __tablename__ = 't_roles'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(23), unique=True)
+    user = db.relationship('User', backref='role')
+
+
+class User(db.Model):
+    __tablename__ = 't_users'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(23), unique=True)
+    role_id = db.Column(db.Integer, db.ForeignKey('t_roles.id'))
 
 
 @app.route('/')

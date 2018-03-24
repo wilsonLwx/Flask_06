0aa641c866fb4035c22f69187bb38e9016a9e2ad
wilsonLWX <15000108650@163.com>
2018-03-24 13:36:05 +0800

    add

--- flask_py2/demo03.py
+++ flask_py2/demo03.py
@@ -7,8 +7,8 @@ from flask_migrate import Migrate, MigrateCommand
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI '] = 'mysql://root:mysql@127.0.0.1/flask_py2'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS '] = False
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:mysql@127.0.0.1/flask_py2'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 app.debug = True
 manager = Manager(app)
